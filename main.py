260524fe0e510dd28b5586ae7a8ff3bc264a5053
Puuggordo <williamliu254@gmail.com>
2026-08-05 20:53:44 +1000

    test with last 15 min data

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ async def db_purge_old():
 async def db_get_week_ago_history(facility_id: str):
     now = time.time()
 
-    start_time = now - (7 * 86400)
-    end_time = now - (6 * 86400)
+    start_time = now - (15 * 60)
+    end_time = now - (0 * 60)
 
     async with make_client() as client:
         result = await client.execute(
